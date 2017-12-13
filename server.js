6d3dcf074cb7b847a8c2931c9d6af132ebae00b3
Kristina Crane <Kristina Crane>
2017-12-13 11:19:31 -0700

    Inital Commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs')
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+app.use((req, res, next) => {
+
+    var now= new Date().toString();
+    var log=`${now}:${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFileSync('server.log',log+ '\n', (err)=>
+    {
+        console.log('Unable to append to server.log.') 
+    });
+    next();
+});
+/** 
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+})
+*/
+app.use(express.static(__dirname+'/public'))
+
+//HBS helpers
+hbs.registerHelper('getCurrentYear',() => { return new Date().getFullYear()});
+hbs.registerHelper('screamIt',(text) => {
+    return text.toUpperCase();
+})
+
+///Route handler
+app.get('/',(request,response) => {
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        message: 'Welcome to my website'
+    });  //Views is the default directory
+});
+
+app.get('/about',(req,res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });  //Views is the default directory
+})
+
+app.get('/bad',(req,res) => {
+    res.send({
+        errorMessage: 'An error has occured'
+    })
+})
+app.listen(3000);
